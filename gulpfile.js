a1c3a9f8c7276bdbfb10694fa5e82c7aeb682dfb
shynkarenkoa <shynkarenko.js@gmail.com>
2016-03-26 15:33:00 +0200

    testing changes postcss

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,22 @@ const gulp = require("gulp")
     , jade = require("gulp-jade")
     , less = require("gulp-less")
     , postcss = require("gulp-postcss")
+    , debug = require("gulp-debug")
 
 gulp.task("jade", () =>
 
-    gulp.src("*.jade")
+    gulp.src("./jade/*.jade")
         .pipe(jade())
-        .pipe(gulp.dest("./"))
+        .pipe(gulp.dest("./public/"))
+        .pipe(debug())
 )
 
 gulp.task("less", () =>
 
-    gulp.src("*.less")
+    gulp.src("./less/*.less")
         .pipe(less())
-        .pipe(gulp.dest("./less/"))
+        .pipe(gulp.dest("./less/css/"))
+        .pipe(debug())
 )
 
 gulp.task("postcss", () =>
@@ -24,16 +27,19 @@ gulp.task("postcss", () =>
     let processors = [ "cssnext", "autoprefixer" ]
         .map(name => require(name)())
 
-    return gulp.src("*.css")
+    return gulp.src("./less/css/*.css")
         .pipe(postcss(processors))
-        .pipe(gulp.dest("./post/"))
+        .pipe(gulp.dest("./public/css/"))
+    // .pipe( postcss([ require('autoprefixer') ]) )
+
+        .pipe(debug())
 })
 
 gulp.task("watch", () =>
 {
-    gulp.watch("*.less", [ "less" ])
-    gulp.watch("*.css", [ "postcss" ])
-    gulp.watch("*.jade", [ "jade" ])
+    gulp.watch("./jade/*.jade", [ "jade" ])
+    gulp.watch("./less/*.less", [ "less" ])
+    gulp.watch("./less/css/*.css", [ "postcss" ])
 })
 
 gulp.task("default", [ "watch" ])
